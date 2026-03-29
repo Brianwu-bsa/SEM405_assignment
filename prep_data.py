7884197395a0ca3fb0e28cead292db0c9c32db3b
Brian <brianwu.bsa@gmail.com>
2026-03-28 21:52:28 -0400

    Added new NAS100 dataset which has a longer start. Added prep_data.py which generated the RTH

---
+++ prep_data.py
@@ -0,0 +1,50 @@
+"""
+Note the time zone for the dataset which is US Eastern Time (EST
+Regular Trading Hours are 9:30 to 16:00
+Premarket (PM): 18:00 to 9:30
+Aftermarket (AM): 16:00 t0 17:00
+
+From 17:00 to 18:00 the stock exchange closes down temporarily for maintenance
+
+"""
+
+import pandas as pd
+import time
+
+"""
+Author: Brian 
+Written: 2026-03-18
+"""
+
+
+def preprocess_data(input_filepath, start_date):
+    df = pd.read_csv(input_filepath, sep="\t")
+    df.columns = df.columns.str.lower()
+
+    df['datetime'] = pd.to_datetime(df['datetime'], format='%Y.%m.%d %H:%M:%S')
+    df['datetime'] = df['datetime'].dt.tz_localize('UTC').dt.tz_convert('America/New_York')
+    df.set_index('datetime', inplace=True)
+    df = df.sort_index()
+
+    df = df.loc[start_date:]
+    df_rth = df.between_time("09:30", "16:00").copy()
+
+    # 5. Keep only OHLC and convert to float32 (Drops Volume & TickVolume)
+    ohlc_cols = ['open', 'high', 'low', 'close']
+    df_rth = df_rth[ohlc_cols].astype('float32')
+
+    # 6. Save the processed data
+    df_rth.to_csv('NAS100_1min_RTH.csv')
+    df_rth.to_parquet('NAS100_1min_RTH.parquet')
+
+    print(f"Processing complete!")
+    print(f"Total RTH bars saved: {len(df_rth)}")
+
+    return df_rth
+
+
+if __name__ == "__main__":
+    # Replace 'raw_nas100_data.csv' with the actual name of your downloaded file
+    processed_df = preprocess_data('nas100_1min_ETH.csv', "2022-01-01")
+
+    print(processed_df.head())
